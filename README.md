7b3c206a7b3dfd5d981e470c13ffc014366aedb9
Jon Hardman <jonhardman@gmail.com>
2015-07-22 10:10:06 +0100

    JH Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-//Todo
+As simple MVC boilerplate project setup
+## Local Development with Grunt
+Assuming Node.js is already installed.
+### Setup
+Run the following commands once:
+```
+If grunt is not installed: npm install -g grunt-cli
+npm install - installs the project dependencies required
+```
+### Run Dev Server  in Browser
+Execute following commands to start the dev server:
+```
+grunt server
+```
--- static_server.js
+++ static_server.js
@@ -1,23 +1,22 @@
-// Usage see http://scn.sap.com/community/developer-center/front-end/blog/2014/01/05/sapui5-sdk-on-nodejs
 var express = require('express'),
 	open = require('open');
 	app = express(),
 	port = process.env.PORT || 8877,
-	sapui5 = '/UI5SplitApp-Boilerplate'
+	urlPath = '/App-Boilerplate'
 	url = 'http://localhost:' + port + sapui5,// + "/latest";
 	year = 60 * 60 * 24 * 365 * 1000;
- 
+
 // Use compress middleware to gzip content
 app.use(express.compress());
- 
+
 //set default mime type to xml for ".library" files
 express.static.mime.default_type = "text/xml";
- 
+
 // Serve up content directory showing hidden (leading dot) files
-app.use(sapui5,express.static(__dirname, { maxAge: year, hidden: true }));
+app.use(urlPath,express.static(__dirname, { maxAge: year, hidden: true }));
 // enable directory listening
-app.use(sapui5,express.directory(__dirname));
- 
+app.use(urlPath,express.directory(__dirname));
+
 app.listen(port);
 open(url); //open in default browser
 console.log("Static file server running at\n  => " + url + " \nCTRL + C to shutdown");
